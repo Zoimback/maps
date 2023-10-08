e2f0c5e722b3343de3636ef8a1c3a2fe1defa804
Zoimback <rgalexmv@gmail.com>
2023-10-09 00:31:26 +0200

    Añadido carpeta Motor(Rust)

---
+++ motor/target/debug/deps/motor-0224f28771a98959.d
@@ -0,0 +1,5 @@
+/home/alex/Rust/maps/motor/target/debug/deps/motor-0224f28771a98959: src/main.rs
+
+/home/alex/Rust/maps/motor/target/debug/deps/motor-0224f28771a98959.d: src/main.rs
+
+src/main.rs:
---
+++ motor/target/debug/motor.d
@@ -0,0 +1,1 @@
+/home/alex/Rust/maps/motor/target/debug/motor: /home/alex/Rust/maps/motor/src/main.rs
